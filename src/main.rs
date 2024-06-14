cb026cfa3371e2b48af030b33d4091033de22e73
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-06-14 18:57:33 -0400

    match statement for commands

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,11 @@ fn main() {
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    match stdin.read_line(&mut input) {
+        Ok(_) => {
+            input = input.trim().to_string();
+            println!("{input}: command not found");
+        }
+        Err(error) => println!("error: {error}"),
+    }
 }
