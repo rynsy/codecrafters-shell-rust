239d31fe598b3f8be51aba99f8e3227bb73a58b1
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-06-14 19:18:57 -0400

    forgot the print

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::io::{self, Write};
 fn process_input(cmd: String) {
     let parts: Vec<&str> = cmd.split(' ').collect();
     if let ["exit", exit_code] = parts.as_slice() {
-        println!("Exit called: {exit_code}");
         std::process::exit(exit_code.parse::<i32>().unwrap());
     //    } else if let ["echo", args @ ..] = parts.as_slice() {
     //        println!("{}", args.join(" "));
