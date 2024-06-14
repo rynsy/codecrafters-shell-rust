408db6384af504b2a7abf594c093a57dd31c3ef6
Ryan Lindsey <ryan.lindsey44@gmail.com>
2024-06-14 19:17:23 -0400

    exit cmd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn process_input(cmd: String) {
+    let parts: Vec<&str> = cmd.split(' ').collect();
+    if let ["exit", exit_code] = parts.as_slice() {
+        println!("Exit called: {exit_code}");
+        std::process::exit(exit_code.parse::<i32>().unwrap());
+    //    } else if let ["echo", args @ ..] = parts.as_slice() {
+    //        println!("{}", args.join(" "));
+    } else {
+        println!("{}: command not found", parts[0]);
+    }
+}
+
 fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
@@ -9,8 +21,8 @@ fn main() {
         io::stdout().flush().unwrap();
         match stdin.read_line(&mut input) {
             Ok(_) => {
-                input = input.trim().to_string();
-                println!("{input}: command not found");
+                let cmd = input.trim().to_string().clone();
+                process_input(cmd);
                 input = String::new();
             }
             Err(error) => println!("error: {error}"),
